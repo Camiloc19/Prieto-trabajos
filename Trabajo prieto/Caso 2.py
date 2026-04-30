97cf3405d2b5455da6f671cc9a23b184b2633c80
Camiloc19 <jcruzvelrno@gmail.com>
2026-04-30 08:11:50 -0500

    nuevo

--- Trabajo prieto/Caso 2.py
+++ Trabajo prieto/Caso 2.py
@@ -1,6 +1,6 @@
 print("Caso 2")
 
-# VALIDAR MES
+
 while True:
     mes = int(input("Ingrese el mes (1-12): "))
     if 1 <= mes <= 12:
@@ -8,7 +8,7 @@ while True:
     else:
         print("FECHA INCORRECTA.")
 
-# ASIGNAR NOMBRE Y DÍAS
+
 if mes == 1:
     nombre_mes = "Enero"; dias_max = 31
 elif mes == 2:
@@ -34,7 +34,7 @@ elif mes == 11:
 elif mes == 12:
     nombre_mes = "Diciembre"; dias_max = 31
 
-# VALIDAR DÍA
+
 while True:
     dia = int(input("Ingrese el día: "))
     if 1 <= dia <= dias_max:
@@ -42,7 +42,7 @@ while True:
     else:
         print("FECHA INCORRECTA.")
 
-# VALIDAR AÑO
+
 while True:
     año = int(input("Ingrese el año: "))
     if año > 0:
@@ -50,27 +50,29 @@ while True:
     else:
         print("Error de dato, intente de nuevo")
 
-# EVENTO
-nombre_evento = input("Ingrese el nombre del evento: ")
 
-# VALIDAR HORAS
+    nombre_evento = input("Ingrese el nombre del evento: ")
+    
+
+
 while True:
     hora_inicio = int(input("Ingrese hora de inicio (0-23): "))
     minutos_inicio = int(input("Ingrese minutos de inicio (0-59): "))
-    if 0 <= hora_inicio <= 23 and 0 <= minutos_inicio <= 59:
+    if 0 >= hora_inicio <= 23 and 0 >= minutos_inicio <= 59:
         break
     else:
         print("Hora inválida.")
 
 while True:
+    print("Utilize el formato de 24 horas porfavor")
     hora_fin = int(input("Ingrese hora de fin (0-23): "))
     minutos_fin = int(input("Ingrese minutos de fin (0-59): "))
-    if 0 <= hora_fin <= 23 and 0 <= minutos_fin <= 59:
+    if 0 >= hora_fin <= 23 and 0 <= minutos_fin <= 59:
         break
     else:
         print("Hora inválida.")
 
-# PERIODO
+
 if hora_inicio < 12 and hora_fin < 12:
     periodo = "AM"
 elif hora_inicio >= 12 and hora_fin >= 12:
@@ -78,8 +80,8 @@ elif hora_inicio >= 12 and hora_fin >= 12:
 else:
     periodo = "AM/PM"
 
-# SALIDA
-print("\n--- DATOS DEL EVENTO ---")
+
+print("\n DATOS DEL EVENTO ")
 print(f"FECHA: {dia} de {nombre_mes} del {año}")
 print(f"NOMBRE: {nombre_evento}")
 print(f"HORARIO: {hora_inicio}:{minutos_inicio} / {hora_fin}:{minutos_fin} {periodo}")
